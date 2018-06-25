d617eb9900956842018655539a328e2af19a0c3a
Grzegorz Kalarus <kalarus.g@gmail.com>
2018-06-25 13:23:00 +0200

    key

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyB0G2yin03UP_gZKBDUQGDykSTNns79NPQ';
+const API_KEY = 'AIzaSyAf7D6aRVxMMBHYcX8YRIRueBw6K2wRj9w';
 
 
 
